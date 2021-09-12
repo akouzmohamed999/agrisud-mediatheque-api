70f2a5f13fc3e8028c67a737c4c884a34db8a9bb
Mohamed AKOUZ <makouz@norsys.fr>
2021-09-12 14:55:55 +0100

    feat(trans): adding int cloud context for sharing

--- src/main/java/org/agrisud/mediathequeapi/clouddao/EventCloudDao.java
+++ src/main/java/org/agrisud/mediathequeapi/clouddao/EventCloudDao.java
@@ -1,60 +1,68 @@
 package org.agrisud.mediathequeapi.clouddao;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-
 import org.aarboard.nextcloud.api.NextcloudConnector;
 import org.aarboard.nextcloud.api.filesharing.Share;
 import org.aarboard.nextcloud.api.filesharing.SharePermissions;
 import org.aarboard.nextcloud.api.filesharing.SharePermissions.SingleRight;
 import org.aarboard.nextcloud.api.filesharing.ShareType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 @Repository
 public class EventCloudDao {
 
     @Autowired
     NextcloudConnector connector;
-    
+
+    @Value("${cloud.server.name}")
+    String serverName;
+
+    @Value("${cloud.server.download-url}")
+    String downloadUrl;
+
 
     public List<String> getEventFolders() {
-    	// TODO: why this /category is static
+        // TODO: why this /category is static
         return connector.listFolderContent("/category");
     }
 
-	public void createFolder(String path) {
-		connector.createFolder(path);
-	}
-	
-	public InputStream getFile(String path) throws IOException {
-		return connector.downloadFile( path);
-	}
-	
-	public void upLoadFile(File file, String path) {
+    public void createFolder(String path) {
+        connector.createFolder(path);
+    }
+
+    public InputStream getFile(String path) throws IOException {
+        return connector.downloadFile(path);
+    }
+
+    public void upLoadFile(File file, String path) {
         connector.uploadFile(file, path);
     }
 
-	public Boolean isFolderExist(String path) {
-		return connector.folderExists(path);
-	}
+    public Boolean isFolderExist(String path) {
+        return connector.folderExists(path);
+    }
 
-	public void deleteFolder(String pathFolder) {
-		connector.deleteFolder(pathFolder);
-	}
+    public void deleteFolder(String pathFolder) {
+        connector.deleteFolder(pathFolder);
+    }
 
-	public void deleteFile(String pathFile) {
-		connector.removeFile(pathFile);
-	}
+    public void deleteFile(String pathFile) {
+        connector.removeFile(pathFile);
+    }
 
-	public String doShared(String path) {
-		SharePermissions permissions = new SharePermissions(SingleRight.READ);
-		Share share = connector.doShare(path, ShareType.PUBLIC_LINK, null, false, null, permissions);
-		return share.getUrl();
-	}
-	public void renameFile(String oldPath, String newPath) {
-		connector.renameFile(oldPath, newPath, false);
-	}
+    public String doShared(String path) {
+        SharePermissions permissions = new SharePermissions(SingleRight.READ);
+        Share share = connector.doShare(path, ShareType.PUBLIC_LINK, null, false, null, permissions);
+        return share.getUrl().replace(serverName, downloadUrl);
+    }
+
+    public void renameFile(String oldPath, String newPath) {
+        connector.renameFile(oldPath, newPath, false);
+    }
 }
